26da3ada6d2e3d32ebccfd4b0450444c7461b8d1
Robert Brown <brownjrr@umich.edu>
2024-09-29 21:15:41 -0400

    Updating Stats data to pair better with Players table

--- scripts/player_ratings.py
+++ scripts/player_ratings.py
@@ -22,18 +22,19 @@ aliases = {
     'UNKNOWN': {'D', 'L', 'KB', 'LA', 'BT', 'LD', 'TOG', 'GOC'},
 }
 
+# inverting aliases dictionary
+inv_alias_dict = dict()
+
+for i in aliases:
+    for j in aliases[i]:
+        inv_alias_dict[j] = i
+
+print(f"inv_alias_dict:\n{inv_alias_dict}")
+
 def check_all_pos_mapped():
     """
     Run a check to make sure all positions can be mapped to the aliases dictionary
     """
-    # inverting aliases dictionary
-    inv_alias_dict = dict()
-
-    for i in aliases:
-        for j in aliases[i]:
-            inv_alias_dict[j] = i
-
-    print(f"inv_alias_dict:\n{inv_alias_dict}")
 
     players_df = pd.read_csv("../data/players.csv")
 
@@ -73,10 +74,89 @@ def check_all_pos_mapped():
 
         print()
 
-# def check_all_pos_mapped():
-#     """
-#     Run a check to make sure players with more than one position labeled
-#     """
+def transform_position_col(x):
+    # extracting position label(s) from string
+    positions = re.split(r'[^a-zA-Z]', x)
+    positions = list({inv_alias_dict[i] for i in positions})
+
+    return positions
+
+def preprocess_stats_data():
+    stat_files = glob.glob("../data/PlayerStats/*.csv")
+
+    print(f"stat_files: {stat_files}")
+    
+    dfs = []
+    for stat_file in stat_files:
+        print(f"stat_file: {stat_file}")
+
+        stat_df = pd.read_csv(stat_file)
+
+        before_shape = stat_df.shape
+
+        # drop rows where position is NaN
+        stat_df = stat_df.dropna(subset=['pos'])
+
+        # dropping rows where position value contains "Missed season" or "Did not play" 
+        stat_df = stat_df[(~stat_df['pos'].astype(str).str.contains("Missed season")) & (~stat_df['pos'].astype(str).str.contains("Did not play"))]
+
+        stat_df['pos'] = stat_df['pos'].apply(lambda x: transform_position_col(x))
+        
+        # dropping rows where player has more than one position listed
+        stat_df = stat_df[~(stat_df['pos'].str.len()>1)]
+
+        # converting "pos" column to str
+        stat_df['pos'] = stat_df['pos'].apply(lambda x: x[0])
+        
+        # dropping rows where position = "UNKNOWN"
+        stat_df = stat_df[~(stat_df['pos']=='UNKNOWN')]
+        
+        after_shape = stat_df.shape
+
+        print(f"stat_df.shape (BEFORE|AFTER): {before_shape}|{after_shape}")
+        stat_df.to_csv(stat_file, index=False)
+        print("Finished Saving")
+
+def update_player_positions():
+    """
+    Run this function to update player positions in the stats data. 
+    """
+
+    stat_files = glob.glob("../data/PlayerStats/*.csv")
+
+    for stat_file in stat_files:
+        print(f"stat_file: {stat_file}")
+
+        stat_df = pd.read_csv(stat_file)
+        stat_df = stat_df.dropna(subset="player_id")
+
+        temp_df = stat_df.groupby(['player_id', 'pos']).size()
+        temp_df.name = 'row_count'
+        temp_df = temp_df.to_frame().reset_index()
+        
+        idx = temp_df.groupby(['player_id', 'pos'])['row_count'].idxmax()
+        max_reps_df = temp_df.loc[idx][['player_id', 'pos']]
+
+        player_pos_dict = max_reps_df.set_index(['player_id']).to_dict()['pos']
+        
+        updated_stat_df = stat_df.copy()
+        updated_stat_df['pos'] = updated_stat_df.apply(lambda row: player_pos_dict[row['player_id']], axis=1)
+
+        print(f"Total player_id/pos pairs after: {updated_stat_df.drop_duplicates(['player_id', 'pos']).shape}")
+        print(f"Total player_id/pos pairs before: {stat_df.drop_duplicates(['player_id', 'pos']).shape}")
+
+        assert updated_stat_df.drop_duplicates(['player_id', 'pos']).shape[0] <= stat_df.drop_duplicates(['player_id', 'pos']).shape[0]
+
+        print(f"Before Shape: {stat_df.shape}")
+        print(f"After Shape: {updated_stat_df.shape}")
+
+        assert stat_df.shape == updated_stat_df.shape
+
+        updated_stat_df.to_csv(stat_file, index=False)
+
 
 if __name__ == '__main__':
-    check_all_pos_mapped()
+    # check_all_pos_mapped()
+    # preprocess_stats_data()
+    # update_player_positions()
+    pass
