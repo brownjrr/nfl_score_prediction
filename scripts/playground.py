d8374413c56fbf834c48815e0b8e427b28ef897e
mas <mas@thorium>
2024-10-09 22:35:57 -0400

    Roster processing - intermediate step

---
+++ scripts/playground.py
@@ -0,0 +1,8 @@
+#%%
+import pandas as pd
+
+# First read in the games (output from step 1)
+model_games_df = pd.read_csv('../data/intermediate/games_df.csv')
+roster_df = pd.read_csv('../data/intermediate/roster_df.csv')
+
+#%%
